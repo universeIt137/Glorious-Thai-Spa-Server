d2bcfb479ec1a7d9d5c1f2a65ffd3fb70e3a88b0
ishanrana363 <ishanrana363@gmail.com>
2024-12-01 17:44:38 +0600

    added

--- index.js
+++ index.js
@@ -306,6 +306,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/banner/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bannerCollection.findOne(query);
+            res.send(result);
+        });
+
 
 
         // Send a ping to confirm a successful connection
