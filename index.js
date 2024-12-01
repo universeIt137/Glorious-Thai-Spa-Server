524a0de1c886ee971dfa5c657f50005573a26b9c
ishanrana363 <ishanrana363@gmail.com>
2024-12-01 17:59:47 +0600

    banner section crud operation completed

--- index.js
+++ index.js
@@ -327,6 +327,15 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/banner/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bannerCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
 
 
         // Send a ping to confirm a successful connection
